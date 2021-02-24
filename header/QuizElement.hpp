5573b4364bb0bb8bd2a6f7164eb0ff0a8f6cfcde
Henry Moore <hmoorerg@gmail.com>
2021-02-23 16:19:51 -0800

    Added in the google test main function

---
+++ header/QuizElement.hpp
@@ -0,0 +1,10 @@
+#ifndef QUIZ_ELEMENT_H 
+#define QUIZ_ELEMENT_H 
+
+class QuizElement
+{
+    virtual void display(bool includeAnswer) = 0;
+    virtual void do_quiz() = 0;
+};
+
+#endif // QUIZ_ELEMENT_H 
