06ccbbb1c36c8f9bad12ae19b957a8d57880a61c
Henry Moore <hmoorerg@gmail.com>
2021-03-09 18:42:32 -0800

    Added example.json to the project

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv){
 
     //std::vector<QuizElement*> qVec = CSV.GetQuestions(filename);
 
-    Section SectionOne("Section One", qVec);
+    Section SectionOne("Main", qVec);
     SectionOne.display(true);
     SectionOne.do_quiz();
 
